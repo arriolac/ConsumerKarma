a6e32310aaef81712ef4c7808dac36fe14ad21fd
Chris Arriola <arriolac279@gmail.com>
2012-10-16 19:55:38 -0700

    Can upload new company.

--- server/consumer_karma/consumer_karma/forms.py
+++ server/consumer_karma/consumer_karma/forms.py
@@ -5,7 +5,3 @@ from django import forms
 class CompanyForm(forms.Form):
     name = forms.CharField(max_length=100)
     description = forms.CharField()
-
-    def is_valid(self):
-        # TODO: write form validation check
-        return True
