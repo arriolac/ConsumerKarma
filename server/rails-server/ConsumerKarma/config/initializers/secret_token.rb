ea516c66ac9bae8fb67d6930716aea49243330da
Chris Arriola <arriolac279@gmail.com>
2012-11-23 17:43:27 -0800

    Adding rails project.

---
+++ server/rails-server/ConsumerKarma/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConsumerKarma::Application.config.secret_token = '923880f533e8743f76fc5d9fdcfe3e8fea491fe8ab1470c8c0fd563a90137dbd69b51ee64ffd782ac62210e8858ecbd03c8703d989799346d8d99be00b76bc71'
