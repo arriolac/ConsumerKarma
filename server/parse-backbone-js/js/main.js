12ea115f164f4d731f00b12c344c40a00949201c
Chris Arriola <arriolac279@gmail.com>
2012-11-24 16:40:39 -0800

    Finally, Parse loaded! Had to use require.js shim config.

--- server/parse-backbone-js/js/main.js
+++ server/parse-backbone-js/js/main.js
@@ -7,13 +7,26 @@ require.config({
         underscore: 'libs/underscore-min',
         parse: 'libs/parse-min',
         templates: '../templates'
+    },
+
+    shim: {
+        'parse': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Parse'
+        }
     }
 });
 
 require([
     // Load application module and pass it to definition function
+    'jquery',
+    'underscore',
+    'parse',
     'application',
-], function(Application) {
-    //console.log(Application.random);
-    Application.initialize();
+], function($, _, Parse, Application) {
+    console.log($);
+    console.log(_);
+    console.log(Parse);
+    console.log(Application.random);
+    //Application.initialize();
 });
